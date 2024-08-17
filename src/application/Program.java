1e4a336b644345e8506a407c6b5c637cfe884e9f
Péricles Santos <pericles.programador@gmail.com>
2024-08-17 07:20:09 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Department department = new Department(2, null);
-		Seller seller = new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, department);
-
-		sellerDao.insert(seller);
+		
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Pericles");
+		seller.setEmail("pericles@email.com");
+		sellerDao.update(seller);
 	}
 
 }
